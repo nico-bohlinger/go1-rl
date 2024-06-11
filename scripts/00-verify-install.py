b57e5c5533fbe1711e29c4b7a0362a2eb5f465a5
Florian Golemo <fgolemo@gmail.com>
2024-06-11 15:54:59 -0400

    added verification again

---
+++ scripts/00-verify-install.py
@@ -0,0 +1,21 @@
+import torch
+from omegaconf import OmegaConf
+import hydra
+from legged_gym.env import Env
+
+OmegaConf.register_new_resolver("eval", eval)
+
+NO_ENVS = 10
+
+@hydra.main(config_path="../configs/", config_name="defaults", version_base=None)
+def eval(cfg):
+    cfg.env.num_envs = min(cfg.env.num_envs, NO_ENVS)
+    env = Env(cfg)
+    for i in range(int(NO_ENVS * env.max_episode_length)):
+        actions = 0. * torch.ones(env.num_envs, env.num_actions, device=env.device)
+        obs, _, rew, done, info = env.step(actions)
+    print("Done")
+
+
+if __name__ == "__main__":
+    eval()
