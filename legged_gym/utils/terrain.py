d73edc096c422c7f0e6f0cae63cbbf1eede885f0
Nico <nico.bohlinger@gmail.com>
2024-06-13 16:40:52 +0200

    Harder terrain and better turning

--- legged_gym/utils/terrain.py
+++ legged_gym/utils/terrain.py
@@ -111,10 +111,10 @@ class Terrain:
                                 length=self.width_per_env_pixels,
                                 vertical_scale=self.cfg.vertical_scale,
                                 horizontal_scale=self.cfg.horizontal_scale)
-        slope = difficulty * 0.4
-        step_height = 0.05 + 0.18 * difficulty
-        discrete_obstacles_height = 0.05 + difficulty * 0.2
-        stepping_stones_size = 1.5 * (1.05 - difficulty)
+        slope = difficulty * 0.8
+        step_height = 0.05 + 0.36 * difficulty
+        discrete_obstacles_height = 0.05 + difficulty * 0.4
+        stepping_stones_size = 3.0 * (1.05 - difficulty)
         stone_distance = 0.05 if difficulty==0 else 0.1
         gap_size = 1. * difficulty
         pit_depth = 1. * difficulty
